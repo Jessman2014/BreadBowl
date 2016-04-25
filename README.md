885eea4d6a7e1da18cf7420b49e08048a594cf03
Jesse Dahir-Kanehl <daisysgreenhouse@gmail.com>
2016-04-24 22:03:29 -0500

    added pie chart and sidebar

--- README.md
+++ README.md
@@ -1,29 +1,10 @@
-# Framework7 PhoneGap Application 
-
-> [Framework7](http://www.idangero.us/framework7) is a Mobile UI framework that can be used to build hybrid apps with PhoneGap. This template allows you to get started using Framework7 
-  quickly. 
-  
-  For a more extensive Framework7 sample, see the [one included in their Github project](https://github.com/nolimits4web/Framework7/tree/master/dist)
-  or the [demo apps on their website](http://www.idangero.us/framework7/apps/#.VpQCc5MrKjQ).
-    
-  Also, for an intro to Framework7, check out [this post on the PhoneGap blog](http://phonegap.com/blog/2015/11/30/framework7/).   
+# Bread Bowl
+This is an expense tracking app that allows simple entry of items bought.
 
 
 ## Usage
     
-### PhoneGap CLI
-
-    $ phonegap create my-app --template phonegap-template-framework7
-
-### Cordova CLI
-
-    $ cordova create my-app --template phonegap-template-framework7
-    
-### Desktop
-
-In your browser, open the file:
-
-    /www/index.html
+Built with PhoneGap it works on almost any device. Tested on Android Nexus 5x and in Firefox Browser. 
 
 
   
--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -1,7 +1,9 @@
 // Initialize app
 var myApp = new Framework7({
-    modalTitle: 'Expense Item'
+    modalTitle: 'Expense Item',
+    swipePanel: 'left'
 });
+
 
 
 // If we need to use custom DOM library, let's save it to $$ variable:
@@ -18,28 +20,81 @@ $$(document).on('deviceready', function() {
     console.log("Device is ready!");
 });
 
-var expenseData = localStorage.bb7Data ? JSON.parse(localStorage.bb7Data) : [];
+$$('.panel-close').on('click', function(e) {
+    myApp.closePanel();
+});
 
-$$('.popup').on('open', function () {
+$$('.open-left-panel').on('click', function(e) {
+    // 'left' position to open Left panel
+    myApp.openPanel('left');
+});
+
+var calendarDefault = myApp.calendar({
+    input: '#calendar-default',
+});
+
+var preloadedData = '[{"title":"Festival Foods","amount":"69.99","boughtDate":"2016-04-07","category":"Grocery","id":"1"},{"title":"Ace Hardware","amount":"30.00","boughtDate":"2016-04-21","category":"Household","id":"2"},{"title":"Walgreens","amount":"15.45","boughtDate":"2016-04-18-","category":"Toiletry","id":"3"},{"title":"Walmart","amount":"62.48","boughtDate":"2016-04-24","category":"Clothing","id":"4"},{"title":"Charter","amount":"15.98","boughtDate":"2016-03-05","category":"Internet","id":"5"},{"title":"Piggly Wiggly","amount":"105.48","boughtDate":"2016-03-21","category":"Grocery","id":"6"},{"title":"Menards","amount":"62.74","boughtDate":"2016-03-15","category":"Household","id":"7"},{"title":"Woodmans","amount":"162.54","boughtDate":"2016-03-16","category":"Grocery","id":"8"},{"title":"Festival Foods","amount":"78.45","boughtDate":"2016-04-05","category":"Grocery","id":"9"},{"title":"Walmart","amount":"13.88","boughtDate":"2016-04-23","category":"Household","id":"10"},{"title":"The Hive","amount":"600","boughtDate":"2016-03-27","category":"Rent","id":"11"}]';
+if (!localStorage.bb7Data || localStorage.bb7Data.length == 0) {
+    localStorage.bb7Data = preloadedData;
+}
+var expenseData = JSON.parse(localStorage.bb7Data);
+
+google.charts.load('current', { 'packages': ['corechart'] });
+google.charts.setOnLoadCallback(drawChart);
+function drawChart() {
+
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Category');
+    data.addColumn('number', 'Amount');
+    var categories = {};
+    if (expenseData.length > 0) {
+        for (i = 0; i < expenseData.length; i++) {
+            if (categories[expenseData[i].category]) {
+                categories[expenseData[i].category] += parseFloat(expenseData[i].amount);
+            }
+            else {
+                categories[expenseData[i].category] = parseFloat(expenseData[i].amount);
+            }
+        }
+        
+        
+        data.addRows(Object.keys(categories).length);
+        var i = 0;
+        for (category in categories) {
+            data.setCell(i, 0, category);
+            data.setCell(i, 1, categories[category]);
+            i++;
+        }
+    }
+
+    var options = {
+        title: 'Expenses By Category',
+        is3D: true,
+        legend: {position: 'none'},
+        backgroundColor: { fill:'transparent' }
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+    chart.draw(data, options);
+
+}
+
+$$('.popup').on('open', function() {
     $$('body').addClass('with-popup');
 });
-$$('.popup').on('opened', function () {
+$$('.popup').on('opened', function() {
     $$(this).find('input[name="title"]').focus();
 });
-$$('.popup').on('close', function () {
+$$('.popup').on('close', function() {
     $$('body').removeClass('with-popup');
     $$(this).find('input[name="title"]').blur().val('');
+    $$(this).find('input[name=amount]').val('');
+    $$(this).find('input[name=date]').val('');
 });
-
-// Popup colors
-$$('.popup .color').on('click', function () {
-    $$('.popup .color.selected').removeClass('selected');
-    $$(this).addClass('selected');
-});
-
 
 // Add expense
-$$('.popup .add-expense').on('click', function () {
+$$('.popup .add-expense').on('click', function() {
     var title = $$('.popup input[name="title"]').val().trim();
     if (title.length === 0) {
         return;
@@ -48,17 +103,20 @@ $$('.popup .add-expense').on('click', function () {
     if (amount.length === 0 && !isNaN(amount)) {
         return;
     }
-    var color = $$('.popup .color.selected').attr('data-color');
+    var boughtDate = $$('.popup input[name=date]').val().trim();
+    var category = $$('.popup .category-select').val();
+    var now = moment().format();
     expenseData.push({
         title: title,
         amount: amount,
-        color: color,
-        checked: '',
-        id: (new Date()).getTime()
+        boughtDate: boughtDate,
+        category: category,
+        id: now
     });
     localStorage.bb7Data = JSON.stringify(expenseData);
     buildExpenseListHtml();
     myApp.closeModal('.popup');
+    drawChart();
 });
 
 // Build Expense HTML using Template7 template engine
@@ -71,39 +129,29 @@ function buildExpenseListHtml() {
 // Build HTML on App load
 buildExpenseListHtml();
 
-// Mark checked
-$$('.expense-items-list').on('change', 'input', function () {
-    var input = $$(this);
-    var item = input.parents('li');
-    var checked = input[0].checked;
-    var id = item.attr('data-id') * 1;
-    for (var i = 0; i < expenseData.length; i++) {
-        if (expenseData[i].id === id) expenseData[i].checked = checked ? 'checked' : '';
-    }
-    localStorage.bb7Data = JSON.stringify(expenseData);
-});
-
-// Delete item
-$$('.expense-items-list').on('delete', '.swipeout', function () {
-    var id = $$(this).attr('data-id') * 1;
+$$('.select-item-checkbox').on('click', function () {
+    var accordionItem = $$(this).parents('.accordion-item');
+    var id = accordionItem.attr('data-id');
     var index;
     for (var i = 0; i < expenseData.length; i++) {
-        if (expenseData[i].id === id) index = i;
+        if (expenseData[i].id == id) index = i;
     }
-    if (typeof(index) !== 'undefined') {
+    if (typeof (index) !== 'undefined') {
         expenseData.splice(index, 1);
         localStorage.bb7Data = JSON.stringify(expenseData);
+        accordionItem.remove();
+        drawChart();
     }
 });
 
 // Update app when manifest updated 
 // http://www.html5rocks.com/en/tutorials/appcache/beginner/
 // Check if a new cache is available on page load.
-window.addEventListener('load', function (e) {
-    window.applicationCache.addEventListener('updateready', function (e) {
+window.addEventListener('load', function(e) {
+    window.applicationCache.addEventListener('updateready', function(e) {
         if (window.applicationCache.status === window.applicationCache.UPDATEREADY) {
             // Browser downloaded a new app cache.
-            myApp.confirm('A new version of Bread Bowl is available. Do you want to load it right now?', function () {
+            myApp.confirm('A new version of Bread Bowl is available. Do you want to load it right now?', function() {
                 window.location.reload();
             });
         } else {
@@ -111,3 +159,5 @@ window.addEventListener('load', function (e) {
         }
     }, false);
 }, false);
+
+
